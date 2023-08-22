8b1815491bdf3fa1b0b81cf1b85481691258b438
Taylor Hulsmans <thulsmans.1133@gmail.com>
2023-08-21 20:36:08 -0400

    add local node modal

--- src/components/organisms/Compositions/Compose.tsx
+++ src/components/organisms/Compositions/Compose.tsx
@@ -7,9 +7,10 @@ import { useGetNodesQuery } from '@src/state/api/nodes';
 import { animated } from '@react-spring/three'
 import { useSpring } from '@react-spring/web'
 import { useDrag, Vector2 } from '@use-gesture/react'
-
+import PopOver from "@components/organisms/PopOver";
 import TooltipIcon from "@components/atoms/TooltipIcon";
 import { IconAdd } from "@icons";
+import NodeCard from "@components/molecules/NodeCard";
 const CompositionNodeCard = ({
     nodeCid,
     colour,
@@ -97,18 +98,69 @@ const CompositionNodeCard = ({
         </animated.mesh>
     )
 }
-const AddNodeModal = () => {
-    return (<>
-    </>)
-}
-const AddNodeButton = () => {
-    const [showAddModal, setShowAddModal] = useState(false)
+const AddNodeModal = ({
+    showAddModal,
+    setShowAddModal,
+    handleAddNode
+}: {
+    showAddModal: boolean,
+    setShowAddModal: Dispatch<SetStateAction<boolean>>,
+    handleAddNode: () => void
+}) => {
+    // get list of local nodes
 
-    const toggleAddModal = () => {
-        setShowAddModal(old => !old)
-    }
+    const { data: nodes, isLoading } = useGetNodesQuery();
+    console.log('mhhhmmmm')
+    // display as options to put into canvas
+    // place to enter dpid for an external desci node
+    // a little 'explore popular repositories' option 
+    return (<PopOver
+        style={{
+            display: 'absolute',
+            top: '0',
+            left: '0',
+            backgroundColor: "#3A3A3ABF",
+        }}
+        isVisible={showAddModal}
+        className="transition-all rounded-lg bg-zinc-100 dark:bg-zinc-900"
+        onClose={() => setShowAddModal(old => !old)}
+    >
+        <p>Add a Local Desci Node</p>
+        {nodes && nodes?.map((node, i) => {
+            return (
+          <NodeCard
+            {...node}
+            node={node}
+            key={`node-card-sidepanel-${node.uuid}`}
+            onClick={() => {
+              console.log('clicked')
+              //dispatch(setPublicView(false));
+              //const targetUrl = `${site.app}${app.nodes}/${RESEARCH_OBJECT_NODES_PREFIX}${node.uuid}`;
+              
+              setTimeout(() => {
+                //navigate(targetUrl);
+                // console.log("navigate", targetUrl);
+                //setIsAddingComponent(false);
+                //etIsAddingSubcomponent(false);
+                //dispatch(toggleResearchPanel(true));
+              });
+              //onClose();
+            }}
+          />
+            )
+        })}
+    </PopOver>
+    )
+}
+const AddNodeButton = ({
+    showAddModal,
+    setShowAddModal
+}: {
+    showAddModal: boolean,
+    setShowAddModal: Dispatch<SetStateAction<boolean>>
+}) => {
     return (
-        <div className="fixed top-[50px] left-[58px] z-0">
+        <div className="fixed top-[50px] left-[58px] z-10">
             <div className="rounded-full w-[50px] h-fit py-2 text-white fill-current bg-black m-8 flex items-center justify-center flex-col gap-3 drop-shadow-lg shadow-lg">
                 <TooltipIcon
                     id="add-component-button"
@@ -121,7 +173,9 @@ const AddNodeButton = () => {
                         <div
                             className="bg-tint-primary hover:bg-tint-primary-dark cursor-pointer w-8 h-8 rounded-full flex items-center justify-center"
                             onClick={() => {
-                                toggleAddModal()
+                                console.log('hi')
+                                setShowAddModal(old => !old)
+                                
                                 //lockScroll();
                                 //setAddFilesWithoutContext(true);
                                 //setIsAddingComponent(true);
@@ -141,6 +195,18 @@ export const Compose = () => {
     const [isDragging, setIsDragging] = useState(false)
     const floorPlane = new THREE.Plane(new THREE.Vector3(0, 0, 0), 0);
     const aspectRef = useRef(null);
+    
+    const [showAddModal, setShowAddModal] = useState(false)
+
+    const toggleAddModal = () => {
+        setShowAddModal(old => !old)
+    }
+    const [nodeCards, setNodeCards] = useState<JSX.Element[] | never >([])
+    const handleAddNode = () => {
+        nodeCards?.push(
+            <CompositionNodeCard nodeCid="hi" activeNode={activeNode} setActiveNode={setActiveNode} colour="blue" setIsDragging={setIsDragging} floorPlane={floorPlane} aspectRef={aspectRef} />
+        )
+    }
 
     useEffect(() => {
         console.log(isLoading, 'isLoading')
@@ -149,16 +215,19 @@ export const Compose = () => {
         }
     }, [isLoading])
     return (<>
-        <AddNodeButton />
+        <AddNodeButton showAddModal={showAddModal} setShowAddModal={setShowAddModal}/>
+        <AddNodeModal handleAddNode={handleAddNode} showAddModal={showAddModal} setShowAddModal={setShowAddModal}/>
         <Canvas
+
             onCreated={({ aspect, size, viewport }) => {
                 aspectRef.current = viewport.factor;
             }}
-            style={{ height: '100vh', width: '100vw' }}
+            style={{ height: '100vh', width: '100vw', zIndex:'1' }}
             camera={{ position: [0, 0, 5], zoom: 1, up: [0, 0, 1], far: 10000 }}
         >
             <ambientLight intensity={0.5} />
             <planeHelper args={[floorPlane, 1000, 'red']} />
+            {nodeCards && nodeCards}
             <CompositionNodeCard nodeCid="hi" activeNode={activeNode} setActiveNode={setActiveNode} colour="blue" setIsDragging={setIsDragging} floorPlane={floorPlane} aspectRef={aspectRef} />
             <CompositionNodeCard nodeCid="there" activeNode={activeNode} setActiveNode={setActiveNode} colour="green" setIsDragging={setIsDragging} floorPlane={floorPlane} aspectRef={aspectRef} />
             <MapControls makeDefault enabled={!isDragging} />
